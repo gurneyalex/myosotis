3dee1ed346a2c8fe50b24042617d0399a911257a
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2010-10-21 10:10:23 +0200

    updated views

--- entities/prix.py
+++ entities/prix.py
@@ -3,11 +3,24 @@ from cubicweb.entities import AnyEntity, fetch_config
 
 class Prix(AnyEntity):
     __regid__ = 'Prix'
+    fetch_attrs, fetch_order = fetch_config(('livres', 'sous', 'deniers', 'florins', 'gros', 'sous_florins', 'denier_florins'))
+
     def dc_title(self):
         monnaie = self.monnaie[0]
+        return u'%s %s' % (self.float_value, monnaie.nom)
+        ## if monnaie.type == u'Livre/Sous/Denier':
+        ##     return u'%s£ %ss %sd %s' % (self.livres or 0 , self.sous or 0, self.deniers or 0, monnaie.nom)
+        ## elif monnaie.type == u'Florin/Gros':
+        ##     return u'%sf %sg %ss %sd %s' % (self.florins or 0, self.gros or 0, self.sous_florins or 0, self.denier_florins or 0, monnaie.nom)
+        ## else:
+        ##     return u'%s %s' % (self.monnaie_or, monnaie.nom)
+
+    @property
+    def float_value(self):
+        monnaie = self.monnaie[0]
         if monnaie.type == u'Livre/Sous/Denier':
-            return u'%s£ %ss %sd %s' % (self.livres or 0 , self.sous or 0, self.deniers or 0, monnaie.nom)
+            return (self.deniers or 0) + 12*((self.sous or 0) + 20*(self.livres or 0))
         elif monnaie.type == u'Florin/Gros':
-            return u'%sf %sg %ss %sd %s' % (self.florins or 0, self.gros or 0, self.sous_florins or 0, self.denier_florins or 0, monnaie.nom)
+            return (self.denier_florins or 0) + 12*((self.sous_florins or 0) + 12*(self.gros or 0) + 20*(self.florins or 0))
         else:
-            return u'%s %s' % (self.monnaie_or, monnaie.nom)
+            return self.monnaie_or or 0
