78961c0ae2c50840c7cf0d292b2624b5c64766be
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2010-11-25 08:30:01 +0100

    move nettoyage.py in migration script

--- migration/0.2.0_Any.py
+++ migration/0.2.0_Any.py
@@ -6,4 +6,17 @@ add_relation_definition('Transaction', 'change', 'Change')
 rql('SET X change Y WHERE X is Compte, Y is Change, Y compte X')
 drop_relation_definition('Change', 'compte', 'Compte')
 drop_attribute('Change', 'dans_compte')
+
+print "Nettoyage"
+query = 'SET X type_mesure NULL, X unite NULL, X provenance_mesure NULL WHERE X is %(etype)s, X quantite NULL'
+for etype in (u'MateriauxParure', u'FabriqueAvecMat', u'AchatMateriaux',):
+    print query % {'etype': etype}
+    rql(query, {'etype': etype})
+
+
+
+commit()
+
+
 #add_cube('simile_timeline')
+
