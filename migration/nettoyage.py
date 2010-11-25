c9f2cea2d7348bafe75b81525e9a221ea85ac4b3
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2010-11-25 08:28:48 +0100

    more verbose script

--- migration/nettoyage.py
+++ migration/nettoyage.py
@@ -1,5 +1,6 @@
 query = 'SET X type_mesure NULL, X unite NULL, X provenance_mesure NULL WHERE X is %(etype)s, X quantite NULL'
 for etype in (u'MateriauxParure', u'FabriqueAvecMat', u'AchatMateriaux',):
+    print query % {'etype': etype}
     rql(query, {'etype': etype})
 
 
