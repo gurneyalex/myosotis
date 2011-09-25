92569d909f7d1f9a1b5f419c1568ebe0e2a343df
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2011-09-25 18:49:32 +0200

    add some colors to the plot

--- entities/compte.py
+++ entities/compte.py
@@ -1,11 +1,14 @@
 #-*- coding: utf-8 -*-
-
+from logilab.mtconverter import xml_escape
 from cubicweb.entities import AnyEntity, fetch_config
 
 class Compte(AnyEntity):
     __regid__ = 'Compte'
     fetch_attrs, fetch_order = fetch_config(['debut', 'fin', 'type_compte', 'inventaire'])
     def dc_title(self):
+        return self.inventaire
+    
+    def dc_long_title(self):
         type_compte = self.type_compte + u' '
         if self.inventaire.lower().startswith('compte'):
             prefix = ''
@@ -15,3 +18,25 @@ class Compte(AnyEntity):
         else:
             prefix = u"Compte de " 
         return u'%s%s%s [%s %s]' % (prefix, type_compte, self.inventaire, self.debut, self.fin)
+
+    def dc_description(self, format='text/plain'):
+        title = self.dc_long_title()
+        rql = 'Any C, COUNT(CDE) GROUPBY C WHERE CDE is Commande, CDE compte C, C eid %(eid)s'
+        rset = self._cw.execute(rql, {'eid': self.eid})
+        if not rset:
+            nb_cdes = 0
+        else:
+            nb_cdes = rset[0][1]
+        rql = 'Any C, COUNT(CDE) GROUPBY C WHERE CDE is Transaction, CDE compte C, C eid %(eid)s'
+        rset = self._cw.execute(rql, {'eid': self.eid})
+        if not rset:
+            nb_trans = 0
+        else:
+            nb_trans = rset[0][1]
+        description = '%d commandes, %d transactions' % (nb_cdes, nb_trans)
+        if format == 'text/plain':
+            return u'\n\n'.join([title,
+                                description])
+        elif format == 'text/html':
+            return u'<h2>%s</h2><p>%s</p>' % (xml_escape(title), xml_escape(description))
+        return u''
--- views/timeline.py
+++ views/timeline.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 from logilab.mtconverter import xml_escape
 
 from cubicweb.web.views import timeline
-from cubicweb.selectors import adaptable
+from cubicweb.selectors import adaptable, is_instance
 
 class MyosotisTimeline(timeline.TimelineView):
     __regid__ = 'myosotis.timeline'
     __select__ =  adaptable('ICalendarable')
+    widget_class = 'MyosotisTimelineWidget'
     jsfiles = timeline.TimelineView.jsfiles + ('cubicweb.myosotis.js',)
 
     def render_url(self, loadurl, tlunit=None): # copied to avoid loadtype=auto
@@ -17,7 +19,7 @@ class MyosotisTimeline(timeline.TimelineView):
         else:
             additional = u''
         self.w(u'<div class="widget" cubicweb:wdgtype="%s" '
-               u'cubicweb:loadurl="%s" %s >' %
+               u'cubicweb:loadurl="%s" style="height:400px;" %s >' %
                (self.widget_class, xml_escape(loadurl),
                 additional))
         self.w(u'</div>')
@@ -29,4 +31,15 @@ class MyosotisTimeline(timeline.TimelineView):
         year, month, day = min_start_date.year, min_start_date.month-1, min_start_date.day
         self.w(u'<script type="text/javascript">jQuery(document).ready(function(){setMinVisibleDate(new Date(%d, %d, %d));});</script>' % (year, month, day))
 
-
+class MyosotisTimelineJson(timeline.TimelineJsonView):
+    __select__ = adaptable('ICalendarable') & is_instance('Compte')
+    colors = {u'trésorerie': 'gold',
+              u'hôtel du comte': 'skyblue',
+              u'hôtel de la comtesse': 'deeppink',
+              u'compte de voyage': 'green',
+              u'nul': 'grey',
+              }
+    def build_event(self, entity):
+        event =  super(MyosotisTimelineJson, self).build_event(entity)
+        event['color'] = self.colors[entity.type_compte.lower()]
+        return event
